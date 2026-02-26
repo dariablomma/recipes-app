f66e96b525979f59412c06e1b89ba46bd231195b
Daria Kozina <daria.kozina@uco.co.il>
2026-02-26 21:47:10 +0300

    created repositories

---
+++ backend/src/main/java/com/daria/recipe/app/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.daria.recipe.app.repository;
+
+import com.daria.recipe.app.entity.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
