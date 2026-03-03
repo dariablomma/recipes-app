3d2bf4fc370435d2e4edbe756ba88e11ab72633e
Daria Kozina <daria.kozina@uco.co.il>
2026-03-03 16:03:03 +0300

    fixed maooers - convertation of list of objects into list of ids, converation of nested object field

--- backend/src/main/java/com/daria/recipe/app/mapper/CategoryMapper.java
+++ backend/src/main/java/com/daria/recipe/app/mapper/CategoryMapper.java
@@ -1,9 +1,9 @@
 package com.daria.recipe.app.mapper;
 
-import com.daria.recipe.app.dto.SignUpRequest;
-import com.daria.recipe.app.dto.UserResponse;
+import com.daria.recipe.app.dto.CategoryRequest;
+import com.daria.recipe.app.dto.CategoryResponse;
+import com.daria.recipe.app.entity.Category;
 import com.daria.recipe.app.entity.Recipe;
-import com.daria.recipe.app.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,17 +11,14 @@ import java.util.Collection;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
-    @Mapping(target = "password", ignore = true)
-    User toEntityWithoutPassword(SignUpRequest request);
+public interface CategoryMapper {
+    Category toEntity(CategoryRequest request);
 
     @Mapping(source = "recipes", target = "recipeIds")
-    UserResponse toResponse(User user);
+    CategoryResponse toResponse(Category category);
 
     default List<Long> mapRecipesToIds(Collection<Recipe> recipes) {
-        if (recipes == null) {
-            return List.of();
-        }
+        if (recipes == null) return List.of();
         return recipes.stream().map(Recipe::getId).toList();
     }
 }
--- backend/src/main/java/com/daria/recipe/app/mapper/RecipeMapper.java
+++ backend/src/main/java/com/daria/recipe/app/mapper/RecipeMapper.java
@@ -4,10 +4,15 @@ import com.daria.recipe.app.dto.RecipeCreateRequest;
 import com.daria.recipe.app.dto.RecipeCreateResponse;
 import com.daria.recipe.app.entity.Recipe;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
     Recipe toEntity(RecipeCreateRequest request);
 
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "catgory.id", target = "categoryId")
     RecipeCreateResponse toResponse(Recipe recipe);
+
+    // todo: categoryId, userId -> Category, User
 }
